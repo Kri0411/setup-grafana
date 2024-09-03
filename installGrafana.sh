b9620465859451289916414df462e6700f8a5024
Kripanshu Singh <78786063+Kri0411@users.noreply.github.com>
2024-09-03 12:51:27 +0530

    commands for grafana setup and installation
    commands for grafana setup and installation

---
+++ installGrafana.sh
@@ -0,0 +1,9 @@
+echo "Starting the installation of grafana server"
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
+sudo apt-get update
+sudo apt-get install -y grafana
+sudo systemctl start grafana-server
+sudo systemctl enable grafana-server
